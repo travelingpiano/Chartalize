8d57544c3571cc50443574cdf037d603469316fe
travelingpiano <lihsuanlu@gmail.com>
2017-07-18 16:01:42 -0700

    change color scheme to darker, make color scheme consistent

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dallas = User.create(username: "Dallas", password: "over9000");
+kelly = User.create(username: "Kelly", password: "chungki");
+aaron = User.create(username: "Aaron", password: "iamamazing");
+debra = User.create(username: "Debra", password: "smallbutmighty");
+demo_user = User.create(username: "ChartalizeMaster", password: "password");

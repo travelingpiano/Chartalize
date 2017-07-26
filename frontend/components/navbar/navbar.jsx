1f006ae6f01bbaeb162017cac9539ea9fd7ec961
travelingpiano <lihsuanlu@gmail.com>
2017-07-25 19:38:50 -0700

    finish profile dropdown

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -7,6 +7,10 @@ class NavBar extends React.Component {
     this.logout = this.logout.bind(this);
     this.handleCharts = this.handleCharts.bind(this);
     this.handleDataTables = this.handleDataTables.bind(this);
+    this.state = {
+      dropdownActive: false
+    };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
   }
 
   handleCharts(e){
@@ -17,13 +21,31 @@ class NavBar extends React.Component {
     this.props.history.push('/data_tables');
   }
 
+  toggleDropdown(e){
+    this.setState({dropdownActive: !this.state.dropdownActive});
+  }
+
   logout(e){
     this.props.logout();
   }
 
   render(){
     let display;
+
     if(this.props.currentUser){
+      let dropdownDisplay;
+      if(this.state.dropdownActive){
+        dropdownDisplay = (
+          <div className="dropdown-display">
+            <label className="dropdown-label">Hi {this.props.currentUser.username}</label>
+            <button onClick={this.logout} className="logout">Log Out</button>
+          </div>
+        );
+      }else{
+        dropdownDisplay = (
+          <div></div>
+        );
+      }
       display = (
         <div className="navbar">
           <Link to='/data_tables'>
@@ -32,10 +54,12 @@ class NavBar extends React.Component {
           <div className="signin-login">
             <button onClick={this.handleCharts} className="login">Charts</button>
             <button onClick={this.handleDataTables} className="login">Data Tables</button>
-            <button onClick={this.logout} className="logout">Log Out</button>
+            <button onClick={this.toggleDropdown} className="login">
+              <i className="fa fa-user" aria-hidden="true"></i>
+            </button>
+            {dropdownDisplay}
           </div>
         </div>
-
       );
     }else {
       display = (
